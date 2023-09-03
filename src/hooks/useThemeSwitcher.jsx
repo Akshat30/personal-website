de0d45c0dec9f5813b13525e27bca0b1fa033120
Akshat30 <akshat30@gmail.com>
2023-09-02 22:11:31 -0700

    tailwind update + dark more is now default

--- src/hooks/useThemeSwitcher.jsx
+++ src/hooks/useThemeSwitcher.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 const useThemeSwitcher = () => {
     const [theme, setTheme] = useState(localStorage.theme);
-	const activeTheme = theme === 'dark' ? 'light' : 'dark';
+	const activeTheme = theme === 'light' ? 'dark' : 'light';
 
 	useEffect(() => {
 		const root = window.document.documentElement;
