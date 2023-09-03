1d98bc32701b16b9c6fb8b2ad2cff6afc242e669
Akshat30 <akshat30@gmail.com>
2023-09-02 21:09:57 -0700

    --homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { AnimatePresence } from 'framer-motion';
-import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import AppFooter from './components/shared/AppFooter';
-import AppHeader from './components/shared/AppHeader';
-import './css/App.css';
-import UseScrollToTop from './hooks/useScrollToTop';
+import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import AppFooter from "./components/shared/AppFooter";
+import AppHeader from "./components/shared/AppHeader";
+import "./css/App.css";
+import UseScrollToTop from "./hooks/useScrollToTop";
 
-const About = lazy(() => import('./pages/AboutMe'));
-const Contact = lazy(() => import('./pages/Contact.jsx'));
-const Home = lazy(() => import('./pages/Home'));
-const Projects = lazy(() => import('./pages/Projects'));
-const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
-
+const About = lazy(() => import("./pages/AboutMe"));
+const Contact = lazy(() => import("./pages/Contact.jsx"));
+const Home = lazy(() => import("./pages/Home"));
+const Projects = lazy(() => import("./pages/Projects"));
+const ProjectSingle = lazy(() => import("./pages/ProjectSingle.jsx"));
 
 function App() {
-	return (
+  return (
     <AnimatePresence>
       <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <ScrollToTop />
           <AppHeader />
           <Suspense fallback={""}>
