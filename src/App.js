44088cb44043e7d1ef4051c465f1a0db26bfbb45
Akshat30 <akshat30@gmail.com>
2023-09-02 22:31:26 -0700

    forgot import statement lol

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppFooter from "./components/shared/AppFooter";
 import AppHeader from "./components/shared/AppHeader";
 import "./css/App.css";
 import UseScrollToTop from "./hooks/useScrollToTop";
+import { useEffect, useState } from "react";
 
 const About = lazy(() => import("./pages/AboutMe"));
 const Contact = lazy(() => import("./pages/Contact.jsx"));
