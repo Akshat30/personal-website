99cfc70903ee28668f421ad8078999d6dbc155aa
Akshat30 <akshat30@gmail.com>
2023-09-02 20:51:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 	return (
     <AnimatePresence>
       <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-        <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+        <Router basename={process.env.PUBLIC_URL}>
           <ScrollToTop />
           <AppHeader />
           <Suspense fallback={""}>
