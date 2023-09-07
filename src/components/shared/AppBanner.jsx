fcffbb87bd5b0d47afa98496a21c3265fe32446b
Akshat30 <akshat30@gmail.com>
2023-09-06 23:33:18 -0700

    resume change

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -102,8 +102,8 @@ const AppBanner = () => {
           className="flex justify-center"
         >
           <a
-            download="AkshatJain-Resume.pdf"
-            href="https://drive.google.com/uc?export=download&id=11GY4YLWIAZaS8gu9Ew3rnIYCEmef5EoI"
+            download="AkshatJainResume.pdf"
+            href="/files/AkshatJainResume.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
